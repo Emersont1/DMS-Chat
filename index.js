dcbeb334e22a49e552c1617b6cc5ddb2781051ed
Peter Taylor <info@emersont1.co.uk>
2020-10-25 10:31:55 +0000

    reverted last commit

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ slackEvents.on('app_mention', (event) => {
 
         // populate map array here
 
-        await sendQuestion(event, new_response.q, new_response.a);
+        sendQuestion(event, new_response.q, new_response.a);
 
         // move into an event handler
         routinePost(event, new_response.q, new_response.a);
